d2a5f72ec6bf586bfa508575258278ed944c81dc
Winmingle <winmingle@gmail.com>
2025-07-22 14:34:51 +0100

    Update solution.py

--- python/solution.py
+++ python/solution.py
@@ -1,7 +1,13 @@
-[
-  {"input": "1234\n", "expected": "10"},
-  {"input": "99\n", "expected": "18"},
-  {"input": "0\n", "expected": "0"},
-  {"input": "10001\n", "expected": "2"},
-  {"input": "567\n", "expected": "18"}
-]
+import unittest
+from solution import sum_of_digits
+
+class TestSumOfDigits(unittest.TestCase):
+    def test_cases(self):
+        self.assertEqual(sum_of_digits(1234), 10)
+        self.assertEqual(sum_of_digits(99), 18)
+        self.assertEqual(sum_of_digits(0), 0)
+        self.assertEqual(sum_of_digits(10001), 2)
+        self.assertEqual(sum_of_digits(567), 18)
+
+if __name__ == '__main__':
+    unittest.main()
