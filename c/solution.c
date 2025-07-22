37e63a3be03f44e9dc37cf570f196ecb260be4fb
Winmingle <winmingle@gmail.com>
2025-07-22 11:03:44 +0100

    Update solution.c

--- c/solution.c
+++ c/solution.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#!/bin/bash
 
-int add(int a, int b) {
-    return a + b;
+gcc solution.c -o solution.out
+if [ $? -ne 0 ]; then
+  echo "Compilation failed"
+  exit 1
+fi
+
+run_test() {
+  input="$1"
+  expected="$2"
+  output=$(echo "$input" | ./solution.out)
+
+  if [ "$output" == "$expected" ]; then
+    echo "Test passed"
+    return 0
+  else
+    echo "Test failed: expected $expected, got $output"
+    return 1
+  fi
 }
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", add(a, b));
-    return 0;
-}
+run_test "2 3" "5"
+run_test "10 20" "30"
+run_test "0 0" "0"
