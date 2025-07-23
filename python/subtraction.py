34fba2d2192f65dd275b69c5c601caedc41e481c
Winmingle <winmingle@gmail.com>
2025-07-23 17:02:41 +0100

    Update subtraction.py

--- python/subtraction.py
+++ python/subtraction.py
@@ -1,1 +1,30 @@
+# test_solution.py
 
+import unittest
+from solution import subtract
+
+class TestSubtractFunction(unittest.TestCase):
+    def test_positive_numbers(self):
+        self.assertEqual(subtract(10, 3), 7)
+
+    def test_negative_result(self):
+        self.assertEqual(subtract(3, 10), -7)
+
+    def test_zero(self):
+        self.assertEqual(subtract(0, 0), 0)
+
+    def test_negative_numbers(self):
+        self.assertEqual(subtract(-5, -3), -2)
+
+    def test_mixed_signs(self):
+        self.assertEqual(subtract(5, -3), 8)
+        self.assertEqual(subtract(-5, 3), -8)
+
+    def test_float_values(self):
+        self.assertAlmostEqual(subtract(5.5, 2.2), 3.3, places=1)
+
+    def test_large_numbers(self):
+        self.assertEqual(subtract(1_000_000, 999_999), 1)
+
+if __name__ == '__main__':
+    unittest.main()
